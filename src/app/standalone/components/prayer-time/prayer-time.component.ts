1b8b975562e60c24748ad62c2e72092775f52245
Areeb <areeebaizam@gmail.com>
2023-01-15 21:53:06 -0800

    Prayer tab

--- src/app/standalone/components/prayer-time/prayer-time.component.ts
+++ src/app/standalone/components/prayer-time/prayer-time.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from "@angular/material/card";
 
 @Component({
   selector: 'tap-prayer-time',
   standalone: true,
-  imports: [MatCardModule],
   templateUrl: './prayer-time.component.html',
   styleUrls: ['./prayer-time.component.scss']
 })
