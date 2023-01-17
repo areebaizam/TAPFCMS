b148465920a24764b1657ef200b1f4512b0ea2bd
Areeb <areeebaizam@gmail.com>
2023-01-17 14:50:50 -0800

    Contact Page

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  HeadingComponent,
+  ContactUsDetailComponent,
+  FooterCopyrightComponent,
+} from "@tap/standalone/components/";
+import { TopicHeading, topicList, eTopicHeading } from "@tap/shared/models";
 
 @Component({
-  selector: 'tap-contact',
+  selector: "tap-contact",
   standalone: true,
-  imports: [],
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  imports: [
+    HeadingComponent,
+    ContactUsDetailComponent,
+    FooterCopyrightComponent,
+  ],
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent {
-
+  heading: TopicHeading = topicList.filter(
+    (t) => t.type == eTopicHeading.CONTACT
+  )[0];
 }
