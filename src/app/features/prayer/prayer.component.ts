7d5471cc5977603bc8f479b726f2f725cd243cbb
Areeb <areeebaizam@gmail.com>
2023-01-18 22:41:34 -0800

    prayer header

--- src/app/features/prayer/prayer.component.ts
+++ src/app/features/prayer/prayer.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { Subscription, timer } from "rxjs";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { FooterCopyrightComponent } from "@tap/standalone/components/";
 @Component({
-  selector: 'tap-prayer',
+  selector: "tap-prayer",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './prayer.component.html',
-  styleUrls: ['./prayer.component.scss']
+  imports: [RouterLink,MatToolbarModule, MatButtonModule, FooterCopyrightComponent],
+  templateUrl: "./prayer.component.html",
+  styleUrls: ["./prayer.component.scss"],
 })
-export class PrayerComponent {
+export class PrayerComponent implements OnInit, OnDestroy {
+  subscriptions: Subscription = new Subscription();
 
+  date: string = "";
+  time: string = "";
+
+  ngOnInit(): void {
+    this.initListener();
+  }
+
+  initListener() {
+    this.subscriptions.add(
+      timer(0, 1000).subscribe((n) => {
+        this.date = new Date().toLocaleDateString("en-US", {
+          weekday: "long",
+          year: "numeric",
+          month: "short",
+          day: "numeric",
+        });
+        this.time = new Date().toLocaleTimeString();
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
