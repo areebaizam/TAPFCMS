9540e8a6f0fd4c0f70d64ebf0537103907dcda2d
Areeb <areeebaizam@gmail.com>
2023-01-23 02:32:45 -0800

    Set Browser Platform

--- src/app/standalone/components/hero/hero.component.ts
+++ src/app/standalone/components/hero/hero.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core"
+import { Component, Inject } from "@angular/core";
+import { PLATFORM_ID } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { NgIf } from "@angular/common";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import {
@@ -11,11 +14,22 @@ import { TopicHeading, topicList, eTopicHeading } from "@tap/shared/models";
 @Component({
   selector: "tap-hero",
   standalone: true,
-  imports: [MatButtonModule,MatIconModule,HeaderDisplayComponent, HeadingComponent, PrayerTimeComponent],
+  imports: [
+    NgIf,
+    MatButtonModule,
+    MatIconModule,
+    HeaderDisplayComponent,
+    HeadingComponent,
+    PrayerTimeComponent,
+  ],
   templateUrl: "./hero.component.html",
   styleUrls: ["./hero.component.scss"],
 })
 export class HeroComponent {
+  isBrowser: boolean;
+  constructor(@Inject(PLATFORM_ID) platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
   heading: TopicHeading = topicList.filter(
     (t) => t.type == eTopicHeading.WELCOME
   )[0];
